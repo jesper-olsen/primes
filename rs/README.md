9c3ffdef9ce29ecdf7eb92a42725062eec526b34
Jesper Olsen <jesper.olsen@gmail.com>
2022-11-01 07:53:22 -0600

    initial source

---
+++ rs/README.md
@@ -0,0 +1,12 @@
+# primes
+
+```
+% time cargo run --release
+    Finished release [optimized] target(s) in 0.01s
+     Running `target/release/primes`
+n: 10000000, #primes: 664579
+[2, 3, 5, 7, 11]
+cargo run --release  0.08s user 0.02s system 23% cpu 0.456 total
+```
+
+
---
+++ rs/src/main.rs
@@ -0,0 +1,28 @@
+//Sieve of Eratosthenes - calc all primes below n
+fn prime_sieve(n:usize) -> Vec<usize> {
+    let mut l=vec![true;n];
+    l[0]=false;
+    l[1]=false;
+    let q=f64::sqrt(l.len() as f64) as usize + 1;
+    //let q=l.len();
+    for i in 0..q {
+        if l[i] {
+            for y in (i+i..n).step_by(i) {
+                l[y]=false;
+            }
+        }
+    }
+
+    l.iter()
+    .enumerate()
+    .filter(|(_a,b)| **b)
+    .map(|(a,_b)|a)
+    .collect::<Vec<usize>>()
+}
+
+fn main() {
+    let n = usize::pow(10,7);
+    let l=prime_sieve(n);
+    println!("n: {}, #primes: {}",n,l.len());
+    println!("{:?}",l[0..5].to_vec());
+}
